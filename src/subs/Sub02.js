ed180e2e45e64eeb85e71e9e1de9d9f01508d497
syuulee <yuyu00lee@gmail.com>
2022-10-26 17:44:27 +0900

    2022.10.26

--- src/subs/Sub02.js
+++ src/subs/Sub02.js
@@ -48,60 +48,60 @@ const Sub02 = () => {
                                     alt=''
                                 />
                             </figure>
-                            <p className='name'>강수진</p>
-                            <span className='part'>단장 겸 예술감독</span>
+                            <p className='name'>박일</p>
+                            <span className='part'>발레마스터</span>
                         </li>
                         <li>
                             <figure>
                                 <img
                                     src={
                                         process.env.PUBLIC_URL +
-                                        '/images/staff_01.jpg'
+                                        '/images/staff_02.jpg'
                                     }
                                     alt=''
                                 />
                             </figure>
-                            <p className='name'>강수진</p>
-                            <span className='part'>단장 겸 예술감독</span>
+                            <p className='name'>정현옥</p>
+                            <span className='part'>발레마스터</span>
                         </li>{' '}
                         <li>
                             <figure>
                                 <img
                                     src={
                                         process.env.PUBLIC_URL +
-                                        '/images/staff_01.jpg'
+                                        '/images/staff_03.jpg'
                                     }
                                     alt=''
                                 />
                             </figure>
-                            <p className='name'>강수진</p>
-                            <span className='part'>단장 겸 예술감독</span>
+                            <p className='name'>안효진</p>
+                            <span className='part'>발레마스터</span>
                         </li>{' '}
                         <li>
                             <figure>
                                 <img
                                     src={
                                         process.env.PUBLIC_URL +
-                                        '/images/staff_01.jpg'
+                                        '/images/staff_03.jpg'
                                     }
                                     alt=''
                                 />
                             </figure>
-                            <p className='name'>강수진</p>
-                            <span className='part'>단장 겸 예술감독</span>
+                            <p className='name'>이용철</p>
+                            <span className='part'>발레마스터</span>
                         </li>{' '}
                         <li>
                             <figure>
                                 <img
                                     src={
                                         process.env.PUBLIC_URL +
-                                        '/images/staff_01.jpg'
+                                        '/images/staff_04.jpg'
                                     }
                                     alt=''
                                 />
                             </figure>
-                            <p className='name'>강수진</p>
-                            <span className='part'>단장 겸 예술감독</span>
+                            <p className='name'>박나리</p>
+                            <span className='part'>발레마스터</span>
                         </li>
                     </ul>
                 </div>
